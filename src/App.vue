b2e57b2a4f4e23a2a0d8591ddadbbb697e8877c0
Yaoyoyo <964862992@qq.com>
2021-01-28 11:28:35 +0800

    调整项目初始化结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h3>黑马头条</h3>
     </div>
+    <!-- 路由的出口 -->
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  // 取名字方便我们在调试工具中搜索该组件
+  name: 'App'
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// 加载全局样式文件
+import './styles/index.less'
 
+// 配置生产环境提示
+Vue.config.productionTip = false
+// 创建Vue根实例 将router,store配置到根实例中
+// 把App 根组件渲染到#app节点上
 new Vue({
   router,
   store,
